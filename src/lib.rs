54019dd7cb7c240345f656762adcc4dc5e56e330
Bravlin <bravlinleonel@gmail.com>
2020-10-24 23:19:28 -0300

    Inversion of control

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,21 @@
-use std::iter;
+pub mod padding;
 
-fn slices_xor(s1: &[u8], s2: &[u8]) -> Vec<u8> {
-    assert_eq!(s1.len(), s2.len());
+use padding::PaddingError;
 
-    let mut result = Vec::with_capacity(s1.len());
-    for i in 0..s1.len() {
-        result.push(s1[i] ^ s2[i]);
-    }
-
-    result
+pub trait KeyGenerator {
+    fn next_key(&mut self) -> Vec<u8>;
 }
 
-fn execute_rounds<F: Fn(&[u8], &[u8]) -> Vec<u8>>(
+fn execute_rounds<K: KeyGenerator, F: Fn(&[u8], &[u8]) -> Vec<u8>>(
     result: &mut [u8],
     block_size: usize,
-    key: &[u8],
+    key_generator: &mut K,
     function: F,
-    rounds: u32,
+    rounds: usize,
 ) {
     let (mut start, mut middle, mut end): (usize, usize, usize);
     let (mut left, mut right);
+    let mut key;
     let half_block_size = block_size/2;
 
     start = 0;
@@ -33,8 +29,12 @@ fn execute_rounds<F: Fn(&[u8], &[u8]) -> Vec<u8>>(
 
             result[start..middle].copy_from_slice(&right[..]);
             
-            right = function(&right[..], key);
-            left = slices_xor(&left[..], &right[..]);
+            // Produces the next right side
+            key = key_generator.next_key();
+            right = function(&right[..], &key[..]);
+            for i in 0..half_block_size {
+                left[i] ^= right[i];
+            }
             result[middle..end].copy_from_slice(&left[..]);
         }
 
@@ -47,58 +47,64 @@ fn execute_rounds<F: Fn(&[u8], &[u8]) -> Vec<u8>>(
     }
 }
 
-pub fn cipher<F: Fn(&[u8], &[u8]) -> Vec<u8>>(
+pub fn cipher<P, K, F>(
     message: &[u8],
     block_size: usize,
-    key: &[u8],
+    padder: P,
+    key_generator: &mut K,
     function: F,
-    rounds: u32,
+    rounds: usize,
 ) -> Vec<u8>
+where
+    P: Fn(&[u8], usize) -> Vec<u8>,
+    K: KeyGenerator,
+    F: Fn(&[u8], &[u8]) -> Vec<u8>,
 {
     assert!(block_size > 0 && block_size%2 == 0);
 
-    let needed_padding = block_size - message.len() % block_size;
-    let mut result = Vec::with_capacity(message.len() + needed_padding);
-    result.extend_from_slice(message);
-    result.extend(iter::repeat(needed_padding as u8).take(needed_padding));
-
-    execute_rounds(&mut result[..], block_size, key, function, rounds);
+    let mut result = padder(message, block_size);
+    execute_rounds(&mut result[..], block_size, key_generator, function, rounds);
 
     result
 }
 
-pub fn decipher<F: Fn(&[u8], &[u8]) -> Vec<u8>>(
+pub fn decipher<K, F, R>(
     message: &[u8],
     block_size: usize,
-    key: &[u8],
+    key_generator: &mut K,
     function: F,
-    rounds: u32,
-) -> Vec<u8> {
+    rounds: usize,
+    padding_remover: R,
+) -> Result<Vec<u8>, PaddingError>
+where
+    K: KeyGenerator,
+    F: Fn(&[u8], &[u8]) -> Vec<u8>,
+    R: Fn(&mut Vec<u8>) -> Result<(), PaddingError>,
+{
     assert!(block_size > 0 && block_size%2 == 0);
 
     let mut result = Vec::with_capacity(message.len());
     result.extend_from_slice(message);
+    execute_rounds(&mut result[..], block_size, key_generator, function, rounds);
+    padding_remover(&mut result)?;
 
-    execute_rounds(&mut result[..], block_size, key, function, rounds);
-
-    let padding = match result.last() {
-        None => panic!("No padding found."),
-        Some(padding) => *padding,
-    };
-
-    for _ in 1..=padding {
-        match result.pop() {
-            Some(value) if value == padding => continue,
-            _ => panic!("Malformed padding."),
-        }
-    }
-
-    result
+    Ok(result)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use {
+        super::*,
+        padding::pkcs7,
+    };
+
+    struct SimpleKey(String);
+
+    impl KeyGenerator for SimpleKey {
+        fn next_key(&mut self) -> Vec<u8> {
+            self.0.as_bytes().to_vec()
+        }
+    }
 
     fn slices_or(s1: &[u8], s2: &[u8]) -> Vec<u8> {
         let (shortest, longest) = if s1.len() < s2.len() { (s1, s2) } else { (s2, s1) };
@@ -120,9 +126,23 @@ mod tests {
     #[test]
     fn test() {
         let message = b"Hello, World!";
-        let key = b"Password";
-        let ciphered = cipher(&message[..], 16, &key[..], slices_or, 50);
-        let deciphered = decipher(&ciphered[..], 16, &key[..], slices_or, 50);
+        let mut key_generator = SimpleKey(String::from("Password"));
+        let ciphered = cipher(
+            &message[..],
+            16,
+            pkcs7::add_padding,
+            &mut key_generator,
+            slices_or,
+            50,
+        );
+        let deciphered = decipher(
+            &ciphered[..],
+            16,
+            &mut key_generator,
+            slices_or,
+            50,
+            pkcs7::remove_padding
+        ).unwrap();
         assert_eq!(&message[..], &deciphered[..]);
     }
 }
---
+++ src/padding.rs
@@ -0,0 +1,41 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+#[derive(Debug)]
+pub struct PaddingError(String);
+
+impl Display for PaddingError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.0)
+    }
+}
+
+pub mod pkcs7 {
+    use {
+        std::iter,
+        super::PaddingError,
+    };
+
+    pub fn add_padding(message: &[u8], block_size: usize) -> Vec<u8> {
+        let needed_padding = block_size - message.len() % block_size;
+        let mut result = Vec::with_capacity(message.len() + needed_padding);
+        result.extend_from_slice(message);
+        result.extend(iter::repeat(needed_padding as u8).take(needed_padding));
+
+        result
+    }
+
+    pub fn remove_padding(message: &mut Vec<u8>) -> Result<(), PaddingError>{
+        let padding = match message.last() {
+            None => return Err(PaddingError(String::from("No valid padding found."))),
+            Some(padding) => *padding,
+        };
+        for _ in 1..=padding {
+            match message.pop() {
+                Some(value) if value == padding => continue,
+                _ => return Err(PaddingError(String::from("Malformed padding."))),
+            }
+        }
+
+        Ok(())
+    }
+}
